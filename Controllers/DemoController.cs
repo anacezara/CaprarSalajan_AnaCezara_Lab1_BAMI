0897e1fbea7dc490f5f2fd9d03c17433b2f5d008
Căprar-Sălăjan Ana-Cezara <acezara13@gmail.com>
2025-12-15 18:43:04 +0200

    Controller nou cu metode

---
+++ Controllers/DemoController.cs
@@ -0,0 +1,22 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace CaprarSalajan_AnaCezara_Lab1.Controllers
+{
+    public class DemoController : Controller
+    {
+        public string Index()
+        {
+            return "Bun venit în aplicația mea ASP.NET Core MCV!";
+        }
+
+        public string Salut()
+        {
+            return "Salut!";
+        }
+
+        public string MesajPersonalizat(string nume, int varsta)
+        {
+            return $"Salut {nume}, ai varsta de {varsta} ani - acesta este un mesaj personalizat!";
+        }
+    }
+}
